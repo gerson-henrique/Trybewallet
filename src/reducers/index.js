043d4d7262a5010302996301f0d16555e4b5087e
Gero <gersonhoa@gmail.com>
2022-04-08 12:56:48 -0300

    reupload : trybewallet

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,8 +4,6 @@ import { userReducer } from './user';
 import { walletReducer } from './wallet';
 // import wallet from './wallet';
 
-// Configure os seus reducers.
-// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
 
 const reducer = combineReducers({
   user: userReducer,
